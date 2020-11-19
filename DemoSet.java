16a91a0a23cfb8f1910fb9cb849337556a5587ff
KurkWY <72849206+KurkWY@users.noreply.github.com>
2020-11-19 23:41:32 +0800

    Add files via upload

---
+++ DemoSet.java
@@ -0,0 +1,17 @@
+    import java.util.HashSet;
+
+public class DemoSet{
+
+	public static void main(String[] args){
+		HashSet<String> setFruits = new HashSet<String>();
+
+		setFruits.add("rambutan");
+		setFruits.add("jackfruit");
+		setFruits.add("mangosteen");
+		setFruits.add("jackFruit");
+
+		System.out.println(setFruits);
+
+	}
+}
+    
